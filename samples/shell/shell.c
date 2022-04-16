acdcaf11c136fb08c0bba7ae5cbefcc5fd6c7ce9
Christopher Biggs <unixbigot@pobox.com>
2022-04-16 19:49:47 +1000

    Ztacx shell example

---
+++ samples/shell/shell.c
@@ -0,0 +1,15 @@
+/* main.c - Application main entry point */
+
+/*
+ * Copyright (c) 2015-2016 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#define __main__
+#include "ztacx.h"
+#include "ztacx_led.h"
+
+void main(void)
+{
+	printk("Ztacx Shell sample\n");
+}
